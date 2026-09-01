9dc67db9d55b7ea72a76e363ef3002d2ce38e005
Mariam Jangveladze <marryjangveladze@gmail.com>
2026-09-01 22:58:11 +0400

    Format quiet-hours regression tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -57,12 +57,8 @@ def test_same_day_quiet_hours_do_not_disable_the_entire_day():
     settings = Settings(quiet_hours_start=9, quiet_hours_end=17)
     container = build_container(settings)
 
-    assert container.service._is_quiet_hours(
-        "UTC", datetime(2026, 1, 15, 12, 0, tzinfo=UTC)
-    )
-    assert not container.service._is_quiet_hours(
-        "UTC", datetime(2026, 1, 15, 18, 0, tzinfo=UTC)
-    )
+    assert container.service._is_quiet_hours("UTC", datetime(2026, 1, 15, 12, 0, tzinfo=UTC))
+    assert not container.service._is_quiet_hours("UTC", datetime(2026, 1, 15, 18, 0, tzinfo=UTC))
 
 
 def test_equal_quiet_hour_boundaries_are_rejected():
